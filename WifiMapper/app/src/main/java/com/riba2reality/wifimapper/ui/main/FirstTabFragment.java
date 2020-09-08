fda96dd648232460cbfeb5e331f1f8d158fac53b
Mike Gibson <M.J.Gibson@Exeter.ac.uk>
2020-09-08 17:56:03 +0100

    transfered and updated location tracking code to new apps first tab

--- WifiMapper/app/src/main/java/com/riba2reality/wifimapper/ui/main/FirstTabFragment.java
+++ WifiMapper/app/src/main/java/com/riba2reality/wifimapper/ui/main/FirstTabFragment.java
@@ -1,10 +1,23 @@
 package com.riba2reality.wifimapper.ui.main;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -12,6 +25,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,10 +36,16 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.riba2reality.wifimapper.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.content.Context.LOCATION_SERVICE;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class FirstTabFragment extends Fragment  implements OnMapReadyCallback {
+public class FirstTabFragment extends Fragment  implements OnMapReadyCallback, LocationListener {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
@@ -33,7 +53,19 @@ public class FirstTabFragment extends Fragment  implements OnMapReadyCallback {
 
     static final LatLng HAMBURG = new LatLng(53.558, 9.927);
 
+    float updateRange = 1.0f;
+    long updateTimeMilliSecs = 1000;
+
+
+    final static int PERMISSION_ALL = 1;
+    final static String[] PERMISSIONS = {
+            //Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
     private GoogleMap map;
+    MarkerOptions mo;
+    Marker marker;
+    LocationManager locationManager;
 
     public static FirstTabFragment newInstance(int index) {
         FirstTabFragment fragment = new FirstTabFragment();
@@ -57,6 +89,16 @@ public class FirstTabFragment extends Fragment  implements OnMapReadyCallback {
 //        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
 //                .findFragmentById(R.id.map);
 //        mapFragment.getMapAsync(this);
+
+
+        locationManager = (LocationManager) getActivity().
+                getSystemService(LOCATION_SERVICE);
+        mo = new MarkerOptions().position(new LatLng(0, 0)).title("My current location");
+        if (Build.VERSION.SDK_INT >= 23 && !isPermissionGranted()) {
+            requestPermissions(PERMISSIONS, PERMISSION_ALL);
+        } else requestlocation();
+        if (!isLocationEnabled())
+            showAlert(1);
 
 
     }// end of onCreate method
@@ -85,11 +127,15 @@ public class FirstTabFragment extends Fragment  implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"));
-    }
+        //LatLng sydney = new LatLng(-33.852, 151.211);
+        //googleMap.addMarker(new MarkerOptions()
+        //        .position(sydney)
+        //        .title("Marker in Sydney"));
+
+        map = googleMap;
+        marker = map.addMarker(mo);
+
+    }// end of onMapReady function
 
     @Override
     public void onActivityCreated(Bundle state) {
@@ -100,6 +146,117 @@ public class FirstTabFragment extends Fragment  implements OnMapReadyCallback {
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
+
+
     }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        LatLng myCords = new LatLng(location.getLatitude(), location.getLongitude());
+
+        if (map!= null && marker != null) {
+            marker.setPosition(myCords);
+            map.moveCamera(CameraUpdateFactory.newLatLng(myCords));
+
+            String currentTime = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
+            String message = "Time:" + currentTime + "\nLat:" + location.getLatitude() + "\nLong:" + location.getLongitude();
+
+            Toast myToast = Toast.makeText(getActivity().getBaseContext(), message, Toast.LENGTH_SHORT);
+            myToast.show();
+        }
+
+
+    }// end of onLocationChanged
+
+    private void requestlocation() {
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        locationManager.requestLocationUpdates(provider,
+                updateTimeMilliSecs,
+                updateRange,
+                this);
+
+
+
+    }//end of requestlocation
+
+    private boolean isLocationEnabled(){
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ;
+        //|| locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }//end of isLocationEnabled
+
+    private boolean isPermissionGranted(){
+        if(//checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) ==
+            //     PackageManager.PERMISSION_GRANTED ||
+                getActivity().
+                checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED
+        ){
+            Log.v("myLog","Permission is granted");
+            return true;
+        }else{
+            Log.v("myLog","Permission NOT granted");
+            return false;
+        }
+    }// end of isPermissionGranted
+
+    private void showAlert(final int status){
+        String message, title, btnText;
+        if(status == 1){
+            message = "You Location Settings is set to 'OFF'.\nPlease Enable Location to " +
+                    "use this app";
+            title = "Enable Location";
+            btnText = "Location Settings";
+        }else{
+            message = "Please allow this app to access location!";
+            title = "permission access";
+            btnText = "Grant";
+        }
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this.getActivity());
+        dialog.setCancelable(false);
+        dialog.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(btnText, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        if (status == 1) {
+                            Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(myIntent);
+                        } else
+                            requestPermissions(PERMISSIONS, PERMISSION_ALL);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        getActivity().
+                        finish();
+                    }
+                });
+        dialog.show();
+
+    }// end of showAlert
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+
 
 }// end of class
