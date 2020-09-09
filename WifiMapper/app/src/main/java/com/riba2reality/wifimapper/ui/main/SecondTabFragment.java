085ef2022857411c248ae898eace2bde6ffeb4dc
Mike Gibson <m.j.gibson@exeter.ac.uk>
2020-09-09 14:08:07 +0100

    transfered and updated wifi-scanner code to new apps second tab

--- WifiMapper/app/src/main/java/com/riba2reality/wifimapper/ui/main/SecondTabFragment.java
+++ WifiMapper/app/src/main/java/com/riba2reality/wifimapper/ui/main/SecondTabFragment.java
@@ -1,10 +1,20 @@
 package com.riba2reality.wifimapper.ui.main;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,14 +24,25 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.riba2reality.wifimapper.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class SecondTabFragment extends Fragment {
 
+    private WifiManager wifiManager;
+    private ListView listView;
+    private Button buttonScan;
+    private int size = 0;
+    private List<ScanResult> results;
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayAdapter adapter;
+
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-    private PageViewModel pageViewModel;
+//    private PageViewModel pageViewModel;
 
     public static SecondTabFragment newInstance(int index) {
         SecondTabFragment fragment = new SecondTabFragment();
@@ -34,26 +55,84 @@ public class SecondTabFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
-        int index = 1;
-        if (getArguments() != null) {
-            index = getArguments().getInt(ARG_SECTION_NUMBER);
-        }
-        pageViewModel.setIndex(index);
-    }
+
+//        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+//        int index = 1;
+//        if (getArguments() != null) {
+//            index = getArguments().getInt(ARG_SECTION_NUMBER);
+//        }
+//        pageViewModel.setIndex(index);
+
+
+
+
+
+    }// end of onCreate method
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_wifiscanner, container, false);
-        final TextView textView = root.findViewById(R.id.section_label);
-        pageViewModel.getText().observe(this, new Observer<String>() {
+
+//        final TextView textView = root.findViewById(R.id.section_label);
+//        pageViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+
+        buttonScan = root.findViewById(R.id.scanBtn);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                scanWifi();
             }
         });
+
+        listView = root.findViewById(R.id.wifiList);
+        wifiManager = (WifiManager) getActivity().
+                getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if (!wifiManager.isWifiEnabled()) {
+            Toast.makeText(this.getActivity(), "WiFi is disabled ... We need to enable it", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }
+
+        adapter = new ArrayAdapter<>(this.getActivity(), android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+        scanWifi();
+
+
         return root;
+    }// end of onCreateView
+
+    private void scanWifi() {
+        arrayList.clear();
+        getActivity().
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
+        Toast.makeText(this.getActivity(), "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
     }
-}
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            results = wifiManager.getScanResults();
+            getActivity().unregisterReceiver(this);
+
+            for (ScanResult scanResult : results) {
+                //arrayList.add(scanResult.SSID + " - " + scanResult.capabilities);
+                arrayList.add(scanResult.SSID + " - " + scanResult.BSSID);
+                //arrayList.add(scanResult.SSID + " - \""+scanResult. +"\""
+                adapter.notifyDataSetChanged();
+            }
+        };
+    };
+
+
+
+
+}// end of class
