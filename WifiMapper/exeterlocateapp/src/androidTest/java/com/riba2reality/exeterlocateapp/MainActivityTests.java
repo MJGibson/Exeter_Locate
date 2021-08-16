37c92e792eea636fe5a407c94aef0cee449d79d9
Mike Gibson <M.J.Gibson@Exeter.ac.uk>
2021-08-16 22:19:35 +0100

    Got a test working but can be interupted by other tests, also uses code from the thing it is testing, which doesn't seem great.

--- WifiMapper/exeterlocateapp/src/androidTest/java/com/riba2reality/exeterlocateapp/MainActivityTests.java
+++ WifiMapper/exeterlocateapp/src/androidTest/java/com/riba2reality/exeterlocateapp/MainActivityTests.java
@@ -1,34 +1,45 @@
 package com.riba2reality.exeterlocateapp;
 
+import android.app.ActivityManager;
 import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.FlakyTest;
 import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
-import org.junit.Rule;
+import com.riba2reality.exeterlocatecore.TrackerScanner;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+import static androidx.core.content.ContextCompat.startActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-import static org.junit.Assert.assertTrue;
+import static java.lang.Thread.sleep;
 
 @RunWith(AndroidJUnit4.class)
 //@RunWith(MockitoJUnitRunner.class)
 @LargeTest
-public class MainActivityTests {
+public class MainActivityTests extends TestCase {
 
 
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+//    @Rule
+//    public ActivityScenarioRule<MainActivity> mActivityRule = new ActivityScenarioRule(MainActivity.class);
 
 //    @Rule
 //    public GrantPermissionRule mGrantPermissionRule =
@@ -86,21 +97,63 @@ public class MainActivityTests {
     //==============================================================================================
 
 
+    //==============================================================================================
+    private boolean isLocationServiceRunning() {
 
+        ActivityManager activityManager =
+                (ActivityManager) getInstrumentation().getTargetContext().getSystemService(Context.ACTIVITY_SERVICE);
+        if (activityManager != null) {
+
+            for (ActivityManager.RunningServiceInfo service :
+                    activityManager.getRunningServices(Integer.MAX_VALUE)) {
+
+                if (TrackerScanner.class.getName().equals(service.service.getClassName())) {
+                    if (service.foreground) {
+                        return true;
+                    }
+                }
+
+            }// end of looping
+            return false;
+        }// end of if activityManger not null
+        return false;
+    }// end of isLocationServiceRunning
+    //==============================================================================================
 
 
 
     //==============================================================================================
     @Test
+    @FlakyTest
     public void should_displayNoPermission_when_permissionAreDenied() {
 
         //ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
 
 
+
+
         // click start-stop button
         onView(withId(R.id.startStopButton)).perform(click());
+        try {
+            sleep(400);
 
-        assertTrue("Failed to deny permissions",denyPermission());
+            onView(withId(R.id.startStopButton)).check(matches(withText(R.string.start_button_stop_text)));
+
+//        assertTrue("Failed to deny permissions",denyPermission());
+
+            assertTrue("Failed to Start Service",isLocationServiceRunning());
+
+        }
+        catch ( InterruptedException ex){
+
+        }
+
+
+        //onView(withId(R.id.startStopButton)).check(matches(withText(R.string.start_button_stop_text)));
+
+//        assertTrue("Failed to deny permissions",denyPermission());
+
+        //assertTrue("Failed to Start Service",isLocationServiceRunning());
 
 //        onView(withText(R.string.PermissionDeniedToastText))
 //                .inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
@@ -111,13 +164,33 @@ public class MainActivityTests {
     }// end of shouldUpdateTextAfterButtonClick
     //==============================================================================================
 
-//    @Before
-//    public void setUp() throws Exception {
-//        super.setUp();
-//        Intent mLaunchIntent = new Intent(getInstrumentation()
-//                .getTargetContext(), MainActivity.class);
-//        startActivity(getInstrumentation().getTargetContext(),mLaunchIntent, FLAG_ACTIVITY_NEW_TASK);
+
+//    public void testStartServiceOnInit () {
+//        final AtomicBoolean serviceStarted = new AtomicBoolean(false);
+//
+//
+//
+//        setActivityContext(new ContextWrapper(getInstrumentation().getTargetContext()) {
+//            @Override
+//            public ComponentName startService(Intent service) {
+//                Log.v("mockcontext", "Start service: " + service.toUri(0));
+//                if (service.getComponent().getClassName().equals ("net.meridiandigital.tasks.TasksService"))
+//                    serviceStarted.set(true);
+//                return service.getComponent();
+//            }
+//        });
+//        startActivity(getInstrumentation().getTargetContext(),new Intent(), null);
+//        assertTrue ("Service should have been started", serviceStarted.get());
 //    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        Intent mLaunchIntent = new Intent(getInstrumentation()
+                .getTargetContext(), MainActivity.class);
+        mLaunchIntent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+        startActivity(getInstrumentation().getTargetContext(),mLaunchIntent,null );
+    }
 
 
 //    @After
