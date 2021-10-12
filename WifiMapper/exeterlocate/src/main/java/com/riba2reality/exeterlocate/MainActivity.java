a4aa24fbee62933eaca107bf3f2ece686c264b60
Mike Gibson <M.J.Gibson@Exeter.ac.uk>
2021-10-12 15:45:08 +0100

    Bumped to version 18 1.0.17(1.4.6) updated consent page to button to say disagree instead of close to hopefully make the need to scroll towards the bottom more clear.

--- WifiMapper/exeterlocate/src/main/java/com/riba2reality/exeterlocate/MainActivity.java
+++ WifiMapper/exeterlocate/src/main/java/com/riba2reality/exeterlocate/MainActivity.java
@@ -675,7 +675,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        alert.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+        alert.setNegativeButton("Disagree", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 finish();
