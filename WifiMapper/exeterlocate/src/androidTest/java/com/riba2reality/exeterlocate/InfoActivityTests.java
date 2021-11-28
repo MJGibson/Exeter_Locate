609684f4dcb1cc00c88e6958b44d0424478b1cd0
Mike Gibson <M.J.Gibson@Exeter.ac.uk>
2021-11-28 01:38:08 +0000

    tried to make a function to check back key pressing on InfoActivity.java

--- WifiMapper/exeterlocate/src/androidTest/java/com/riba2reality/exeterlocate/InfoActivityTests.java
+++ WifiMapper/exeterlocate/src/androidTest/java/com/riba2reality/exeterlocate/InfoActivityTests.java
@@ -1,8 +1,10 @@
 package com.riba2reality.exeterlocate;
 
+import android.app.Activity;
 import android.content.Context;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.web.webdriver.Locator;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -22,10 +24,14 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.web.assertion.WebViewAssertions.webMatches;
 import static androidx.test.espresso.web.model.Atoms.getCurrentUrl;
 import static androidx.test.espresso.web.sugar.Web.onWebView;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webClick;
+import static com.google.common.truth.Truth.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
 
 @RunWith(AndroidJUnit4.class)
+
 @LargeTest
 public class InfoActivityTests extends TestCase {
 
@@ -48,8 +54,8 @@ public class InfoActivityTests extends TestCase {
 
 
 
-        ActivityScenario<InfoActivity> scenario =
-                ActivityScenario.launch(InfoActivity.class);
+//        ActivityScenario<InfoActivity> scenario =
+//                ActivityScenario.launch(InfoActivity.class);
 
 
     }// end of init
@@ -61,8 +67,8 @@ public class InfoActivityTests extends TestCase {
     public void check_versions(){
 
 
-//        ActivityScenario<InternetMessageActivity> scenario =
-//                ActivityScenario.launch(InternetMessageActivity.class);
+        ActivityScenario<InfoActivity> scenario =
+                ActivityScenario.launch(InfoActivity.class);
 
 
 
@@ -82,15 +88,75 @@ public class InfoActivityTests extends TestCase {
     @Test
     public void check_webView(){
 
+        ActivityScenario<InfoActivity> scenario =
+                ActivityScenario.launch(InfoActivity.class);
+
         onWebView()
                 // Similar to check(matches(...))
                 .check(webMatches(getCurrentUrl(),
                         containsString("ExeterLocateInformationpage.html")));
 
 
+
+
+
+        onWebView()
+                .withElement(findElement(Locator.XPATH, "//a[contains(text(),'What data is collected')]"))
+                //.withElement(findElement(Locator.XPATH, "a[href='.#h.269txh13f89t']"))
+                //.withElement(findElement(Locator.ID, ".#h.269txh13f89t"))
+
+                .perform(webClick());
+
     }// end of checkTitle
     //==============================================================================================
 
+
+    //==============================================================================================
+    @Test
+    public void check_backButton(){
+
+
+        ActivityScenario<InfoActivity> scenario =
+                ActivityScenario.launch(InfoActivity.class);
+
+
+        onWebView()
+                .withElement(findElement(Locator.XPATH, "//a[contains(text(),'What data is collected')]"))
+                //.withElement(findElement(Locator.XPATH, "a[href='.#h.269txh13f89t']"))
+                //.withElement(findElement(Locator.ID, ".#h.269txh13f89t"))
+
+                .perform(webClick());
+
+
+        //Espresso.pressBack();
+        scenario.onActivity( activity -> {
+            // use 'activity'.
+            activity.onBackPressed();
+        });
+
+        // having as we've tried to click, the first back press shouldn't close it...
+
+//        onWebView()
+//                // Similar to check(matches(...))
+//                .check(webMatches(getCurrentUrl(),
+//                        containsString("ExeterLocateInformationpage.html")));
+//
+//        // second one should close it
+//        // causes androidx.test.espresso.NoActivityResumedException: Pressed back and killed the app
+//        //Espresso.pressBack();
+//        scenario.onActivity( activity -> {
+//            // use 'activity'.
+//            activity.onBackPressed();
+//        });
+
+
+
+        assertThat(scenario.getResult().getResultCode()).isEqualTo(Activity.RESULT_CANCELED);
+
+
+
+    }// end of checkTitle
+    //==============================================================================================
 
 
 }// end of InfoActivityTests
