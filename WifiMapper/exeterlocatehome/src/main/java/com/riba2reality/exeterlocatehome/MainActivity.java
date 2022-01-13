7f3344d418044fb544deb11b9630607fb396c9b8
Mike Gibson <M.J.Gibson@Exeter.ac.uk>
2022-01-13 22:06:17 +0000

    added new sub project which will be another front end version of the app called Exeter locate home

---
+++ WifiMapper/exeterlocatehome/src/main/java/com/riba2reality/exeterlocatehome/MainActivity.java
@@ -0,0 +1,14 @@
+package com.riba2reality.exeterlocatehome;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
